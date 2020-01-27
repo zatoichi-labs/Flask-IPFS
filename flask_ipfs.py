cc08e5eceda8fee968a7d9732fa3c02f0a9a4c86
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-27 15:54:35 -0500

    initial commit

---
+++ flask_ipfs.py
@@ -0,0 +1,26 @@
+import ipfshttpclient
+
+from flask import current_app
+
+
+class IPFS:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        cnx_multiaddr = ipfshttpclient.DEFAULT_ADDR
+        # TODO Use app.config.IPFS_GATEWAY_URL and app.config.IPFS_GATEWAY_PORT
+        #      https://github.com/ipfs/go-ipfs/blob/master/docs/config.md#addresses
+        client = ipfshttpclient.connect(cnx_multiaddr)
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['ipfs'] = client
+        app.context_processor(self.context_processor)
+
+    @staticmethod
+    def context_processor():
+        return { 'ipfs': current_app.extensions['ipfs'] }
+
+    def __getattr__(self, name):
+        return current_app.extensions['ipfs']
---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="Flask-IPFS",
+    # NOTE Version managed by bumpversion
+    version="0.1.0-alpha.0",
+    url="https://github.com/zatoichi-labs/Flask-IPFS",
+    license="MIT",
+    author="Zatoichi Labs",
+    author_email="admin@zatoichilabs.com",
+    description="IPFS Plugin for Flask",
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    py_modules=["flask_ipfs"],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "Flask",
+        "ipfshttpclient",
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
