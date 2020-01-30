6fe270247c6927ce5eb8c4a32f22237bf702a91a
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-29 21:02:31 -0500

    feat: Significantly refactored API to allow hosted instances of IPFS
    Hosted instance used is the Interplanetary Fission Heroku plugin

--- demo.py
+++ demo.py
@@ -8,10 +8,52 @@ app.config.update(
     # IPFS integration (use to connect to your IPFS gateway node)
     IPFS_GATEWAY_URL=os.environ.get('IPFS_GATEWAY_URL') or "http://127.0.0.1",
     IPFS_GATEWAY_PORT=int(os.environ.get('IPFS_GATEWAY_PORT') or 5001),
+    # Interplanentary Fission integration (overrides IPFS integration for Heroku)
+    # Add-on: https://elements.heroku.com/addons/interplanetary-fission
+    # Just add these config params (only overrides if all variables have value):
+    INTERPLANETARY_FISSION_URL=os.environ.get('INTERPLANETARY_FISSION_URL'),
+    INTERPLANETARY_FISSION_USERNAME=os.environ.get('INTERPLANETARY_FISSION_USERNAME'),
+    INTERPLANETARY_FISSION_PASSWORD=os.environ.get('INTERPLANETARY_FISSION_PASSWORD'),
 )
 ipfs = IPFS(app)
 
 
 @app.route('/')
-def ipfs_version():
+@app.route('/version')
+def version():
     return f'IPFS Version: {ipfs.version()["Version"]}'
+
+@app.route('/add')
+def add():
+    response = ipfs.add("key")
+    return f'{response}'
+
+@app.route('/get/<cid>')
+def get(cid):
+    response = ipfs.get(cid)
+    return f'{response}'
+
+@app.route('/add_json')
+def add_json():
+    response = ipfs.add_json({"key": "value"})
+    return f'{response}'
+
+@app.route('/get_json/<cid>')
+def get_json(cid):
+    response = ipfs.get_json(cid)
+    return f'{response}'
+
+@app.route('/ls_pins')
+def ls_pins():
+    response = ipfs.ls_pins()
+    return f'{response}'
+
+@app.route('/add_pin/<cid>')
+def add_pin(cid):
+    response = ipfs.add_pin(cid)
+    return f'{response}'
+
+@app.route('/rm_pin/<cid>')
+def rm_pin(cid):
+    response = ipfs.rm_pin(cid)
+    return f'{response}'
--- flask_ipfs.py
+++ flask_ipfs.py
@@ -1,31 +1,100 @@
 import ipaddress
+import json
 from multiaddr import Multiaddr
+import requests
 
 import ipfshttpclient
 
 from flask import current_app
 
 
-def construct_multiaddr(
-    url: str="http://localhost",
-    port: int=5001,
-) -> Multiaddr:
-    http_protocol, host = url.split('://')
+class InterplanetaryFission:
+    def __init__(self, url: str, username: str, password: str):
+        self._url = lambda endpoint: url + endpoint
+        self._auth = (username, password)
 
-    if http_protocol not in ('http', 'https'):
-        raise ValueError(f"Invalid protocol: {http_protocol}")
-    if not isinstance(host, str):
-        ValueError(f"Invalid url: {url}")
+    def add(self, raw_str):
+        response = requests.post(self._url(f'/ipfs'), data=raw_str, auth=self._auth)
+        if response.status_code != 200:
+            raise ValueError(f"Error processing request: {response.text}")
+        return response.text  # returns CID
 
-    try:
-        # method returns either IPv4Address or IPv6Address (or raises ValueError)
-        addr = ipaddress.ip_address(host)
-        ip_protocol = 'ip4' if isinstance(addr, ipaddress.IPv4Address) else 'ip6'
-    except ValueError:
-        ip_protocol = 'dns'
+    def get(self, cid):
+        response = requests.get(self._url(f'/ipfs/{cid}'), auth=self._auth)
+        if response.status_code != 200:
+            raise ValueError(f"Error processing request: {response.text}")
+        return response.text
 
-    addr = Multiaddr(f"/{ip_protocol}/{host}/tcp/{port}/{http_protocol}")
-    return addr
+    def add_json(self, obj):
+        return self.add(json.dumps(obj))
+
+    def get_json(self, cid):
+        return json.loads(self.get(cid))
+
+    def ls_pins(self):
+        response = requests.get(self._url(f'/ipfs/cids'), auth=self._auth)
+        if response.status_code != 200:
+            raise ValueError(f"Error processing request: {response.text}")
+        return response.json()
+
+    def add_pin(self, cid):
+        response = requests.put(self._url(f'/ipfs/{cid}'), auth=self._auth)
+        if response.status_code != 200:
+            raise ValueError(f"Error processing request: {response.text}")
+
+    def rm_pin(self, cid):
+        response = requests.delete(self._url(f'/ipfs/{cid}'), auth=self._auth)
+        if response.status_code != 202:
+            raise ValueError(f"Error processing request: {response.text}")
+
+    def update_pin(self, old_cid, new_cid):
+        self.rm_pin(old_cid)
+        self.add_pin(new_cid)
+
+
+class IpfsHttpClient:
+    def __init__(self, url: str="http://localhost", port: int=5001):
+        http_protocol, host = url.split('://')
+
+        if http_protocol not in ('http', 'https'):
+            raise ValueError(f"Invalid protocol: {http_protocol}")
+        if not isinstance(host, str):
+            ValueError(f"Invalid url: {url}")
+
+        try:
+            # method returns either IPv4Address or IPv6Address (or raises ValueError)
+            addr = ipaddress.ip_address(host)
+            ip_protocol = 'ip4' if isinstance(addr, ipaddress.IPv4Address) else 'ip6'
+        except ValueError:
+            ip_protocol = 'dns'
+
+        addr = Multiaddr(f"/{ip_protocol}/{host}/tcp/{port}/{http_protocol}")
+        self._client = ipfshttpclient.connect(addr)
+
+    def add(self, raw_str):
+        return self._client.add_str(raw_str)
+
+    def get(self, cid):
+        return self._client.object.get(cid)["Data"]
+
+    def add_json(self, obj):
+        return self._client.add_json(obj)  # returns CID
+
+    def get_json(self, cid):
+        return self._client.get_json(cid)
+
+    def ls_pins(self):
+        response = self._client.pin.ls()
+        return list(response['Keys'].keys())
+
+    def add_pin(self, cid):
+        self._client.pin.add(cid)
+
+    def rm_pin(self, cid):
+        self._client.pin.rm(cid)
+
+    def update_pin(self, old_cid, new_cid):
+        self._client.pin.update(old_cid, new_cid)
 
 
 class IPFS:
@@ -34,11 +103,17 @@ class IPFS:
             self.init_app(app)
 
     def init_app(self, app):
-        addr = construct_multiaddr(
-                url=app.config.get('IPFS_GATEWAY_URL', "http://localhost"),
-                port=app.config.get('IPFS_GATEWAY_PORT', 5001),
-            )
-        client = ipfshttpclient.connect(addr)
+        if app.config.get('INTERPLANETARY_FISSION_URL', None):
+            client = InterplanetaryFission(
+                    url=app.config.get('INTERPLANETARY_FISSION_URL'),
+                    username=app.config.get('INTERPLANETARY_FISSION_USERNAME'),
+                    password=app.config.get('INTERPLANETARY_FISSION_PASSWORD'),
+                )
+        else:
+            client = IpfsHttpClient(
+                    url=app.config.get('IPFS_GATEWAY_URL', "http://localhost"),
+                    port=app.config.get('IPFS_GATEWAY_PORT', 5001),
+                )
 
         if not hasattr(app, 'extensions'):
             app.extensions = {}
