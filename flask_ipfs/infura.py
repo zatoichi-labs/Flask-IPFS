071bfc6a572831b14421a43959c5ac41680fb933
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-05 15:42:30 -0500

    refactor: Update for how API works

--- flask_ipfs/infura.py
+++ flask_ipfs/infura.py
@@ -5,17 +5,22 @@ from .base import BaseAPI
 
 class Infura(BaseAPI):
     def __init__(self, url: str, project_id: str=None, project_secret: str=None):
-        self._url = lambda endpoint: url + endpoint
+        self._url = url
         self._auth = (project_id, project_secret)
+        self.pins = []  # Keep track of the pins we know about (temporary until Infura adds this)
+
+    def url(self, endpoint):
+        print(f"{self._url + endpoint}")
+        return self._url + endpoint
 
     def add(self, raw_str):
-        response = requests.post(self._url('/add?pin=true'), files={"fake": (None, raw_str)})
+        response = requests.post(self.url('/add?pin=true'), files={"fake": (None, raw_str)})
         if response.status_code != 200:
             raise ValueError(f"Error processing request: {response.text}")
         return response.json()['Hash']  # returns CID
 
     def get(self, cid):
-        response = requests.get(self._url(f'/get?arg={cid}'))
+        response = requests.get(self.url(f'/cat?arg={cid}'))
         if response.status_code != 200:
             raise ValueError(f"Error processing request: {response.text}")
         return response.text
@@ -27,21 +32,18 @@ class Infura(BaseAPI):
         return json.loads(self.get(cid))
 
     def ls_pins(self):
-        print(self._url(f'/pin/ls'))
-        response = requests.get(self._url(f'/pin/ls'))
-        if response.status_code != 200:
-            raise ValueError(f"Error processing request: {response.text}")
-        return response.json()
+        return self.pins
 
     def add_pin(self, cid):
-        response = requests.get(self._url(f'/pin/add?arg={cid}'))
+        response = requests.get(self.url(f'/pin/add?arg={cid}'))
         if response.status_code != 200:
             raise ValueError(f"Error processing request: {response.text}")
+        self.pins.append(cid)
 
     def rm_pin(self, cid):
-        # Cannot remove pins
-        pass
+        # Cannot remove pins from IPFS
+        self.pins.remove(cid)
 
     def update_pin(self, old_cid, new_cid):
         self.add_pin(new_cid)
-        # Cannot remove pins
+        self.rm_pin(old_cid)
