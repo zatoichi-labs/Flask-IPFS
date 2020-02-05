71138409bd171508ef7ea02a4ef0fd26041b3bed
Bryant Eisenbach <fubuloubu@gmail.com>
2020-02-05 15:45:23 -0500

    Bump version: 0.1.0-beta.0 → 0.1.0-beta.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name="Flask-IPFS",
     # NOTE Version managed by bumpversion
-    version="0.1.0-beta.0",
+    version="0.1.0-beta.1",
     url="https://github.com/zatoichi-labs/Flask-IPFS",
     license="MIT",
     author="Zatoichi Labs",
